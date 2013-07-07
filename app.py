82558e593b76e882fc31205c1796fa49bf9887f4
Mike Zamansky <zamansky@gmail.com>
2013-07-07 15:34:57 -0400

    Z

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 from flask import Flask, request, render_template, url_for, redirect
+import sys
+import popen2
+import pymongo
+import json
 
 app = Flask(__name__)
 app.secret_key="testkey"
@@ -8,6 +12,42 @@ app.secret_key="testkey"
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/getDateRange")
+def getDateRange():
+    cursor = collection.find({'stationName':'W 26 St & 8 Ave'})
+    cursor.sort("timestamp")
+    oldest=cursor[0]['timestamp']
+    cursor.sort("timestamp",-1)
+    newest=cursor[0]['timestamp']
+    range={'min':oldest,'max':newest}
+    return json.dumps(range)
+
+@app.route("/getstationdiff/<station>/<time>")
+def getstationdiff(station=None,time=None):
+    span=time-(1000*60*30)
+    cursor=collection.find({'stationName':station,
+                            'timestamp':{'$gt':span,'$lt':time+1}},{'_id':0})
+    cursor.sort("timestamp")
+    l= [x for x in cursor]
+    dif = l[0]['availableBikes']-l[-1]['availableBikes']
+    return json.dumps(dif)
+
+
+@app.route("/getStations")
+def getStations():
+    stations =  db.command({'distinct':'stations','key':'stationName'})[
+        'values']
+    cursor = collection.find({'stationName':'W 26 St & 8 Ave'})
+    cursor.sort("timestamp")
+    newest=cursor[0]['timestamp']
+    return json.dumps({'stations':stations,'newest':newest})
+
+
+x = popen2.popen2("ssh -N -L 4321:localhost:27017 mongo.zamansky.net")
+connection = pymongo.Connection("localhost",4321)
+db=connection['citibike']
+collection = db['stations']
 
 if __name__=="__main__":
     app.debug=True
--- static/main.js
+++ static/main.js
@@ -1,31 +1,40 @@
 
 DateModel = Backbone.Model.extend({
-    defaults: { date: new Date()}
+    defaults: { date: new Date()},
+    initialize: function() {
+	var that=this;
+	$.getJSON("/getDateRange",function(data) {
+	    that.set('min',data.min);
+	    that.set('max',data.max);
+	    that.get('view').render();
+	});
+    }
+    
 });
 
 
 DateView = Backbone.View.extend({
     el:'#interface',
     initialize: function() {
-	this.render();
+	//_.bindAll(this,"render");
+	//this.model.bind('change',this.render);
+	//this.render();
     },
     events: {
 	"change input[type=range]": "doChangeDate",
 	"mouseup input[type=range]": "render"
     },
     doChangeDate : function(e) {
-	console.log(e.target.value);
 	
 	var tmp = new Date();
 	var d = new Date(parseInt(e.target.value));
 	this.model.set({'date':d});
-	//this.render();
+	//this.render(); // this seems to be better as a mouseup
     },
     render: function() {
 	var date=this.model.get('date')
-	var now=new Date()
-	var max=now.getTime();
-	var min=max-(1000*60*5)
+	var max=this.model.get('max');
+	var min=this.model.get('min')
 	var subs={date:date,min:min,max:max};
 	var t = _.template($("#time_template").html(),subs)
 	this.$el.html(t);
@@ -33,10 +42,53 @@ DateView = Backbone.View.extend({
     }
 });
 
-var d,v;
 
+StationsView = Backbone.View.extend({
+    el:'#stationlist',
+    events: {
+	"change select": "doChangeStation"
+	},
+    doChangeStation:function(e) {
+	var newStation = e.target.value;
+	m = new StationModel(newStation);
+	console.log(m);
+	},
+    render: function() {
+	console.log(this);
+	var subs={stations:this.model.get('stations')};
+	var t = _.template($("#stationlist_template").html(),subs);
+	this.$el.html(t);
+	return this;
+    }
+});
+
+StationsModel = Backbone.Model.extend({
+    url:'/getStations',
+    initialize:function() {
+	var that=this;
+	this.fetch({success:function(data) {
+	    that.get('view').render();
+	    }});
+    }
+
+})
+
+StationModel = Backbone.Model.extend({
+    initialize:function(data) {
+	console.log(data);
+    }
+});
+
+
+
+var d,v;
+var stations,sv;
 $(document).ready(function(){
     d = new DateModel();
     v = new DateView({model:d});
+    d.set('view',v);
+    stations=new StationsModel();
+    sv = new StationsView({model:stations});
+    stations.set('view',sv);
 });
 
